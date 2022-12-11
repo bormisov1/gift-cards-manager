da2f4e4e9be288fb3f2a1b05034933b80b1624f9
elzuha <elzuha2000@gmail.com>
2022-12-11 13:30:17 +0300

    btns work edited

--- frontend/src/views/CardsList.vue
+++ frontend/src/views/CardsList.vue
@@ -4,10 +4,24 @@
       :isDialog="isScanQRDialog"
       @closeDialog="closeScanQRDialog">
     </qr-dialog>
-    <v-toolbar>
-      <v-btn @click="createQR">add</v-btn>
-      <v-btn @click="isScanQRDialog = true">scan</v-btn>
-      <v-btn>logout</v-btn>
+    <v-toolbar flat :elevation="0">
+      <v-row style="justify-content: flex-end;">
+        <v-col cols="auto">
+          <v-btn @click="createQR">
+            <v-icon>add</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col cols="auto">
+          <v-btn @click="isScanQRDialog = true">
+            <v-icon>qr_code_scanner</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col cols="auto">
+          <v-btn @click="logout">
+            <v-icon>logout</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
     </v-toolbar>
     <v-row>
       <v-col
@@ -42,20 +56,25 @@ export default {
   computed: {},
   methods: {
     openGiftCard(giftCardId) {
-      this.$router.push({name: 'gift-card-editing', params: {id: giftCardId}})
+      this.$router.push({name: 'editGiftCard', params: {id: giftCardId}})
     },
     closeScanQRDialog(link) {
       if (link) {
         const splittedLink = link.split('/')
         const giftCardId = splittedLink[splittedLink.length - 1]
-        this.$router.push({name: 'gift-card-editing', params: {giftCardId}})
+        this.$router.push({name: 'editGiftCard', params: {giftCardId}})
       }
       this.isScanQRDialog = false
     },
     async createQR() {
-      const {body} = await fetchRequest('/gift-cards/create', 'POST')
-      if (!body || body.id == null) return
-      this.$router.push({name: 'gift-card-editing', params: {giftCardId: body.id}})
+      //const {body} = await fetchRequest('/gift-cards/create', 'POST')
+      //if (!body || body.id == null) return
+      this.$router.push({name: 'createGiftCard'})
+      // params: {giftCardId: body.id}})
+    },
+    logout() {
+      localStorage.clear()
+      this.$router.push({name: 'auth'})
     }
   },
   components: {QRDialog}
