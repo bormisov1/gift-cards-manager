be8d32cc5a6f6d6d281dce3e7928a5325b65ba51
elzuha <elzuha2000@gmail.com>
2022-12-13 12:08:04 +0300

    cards list view fixed, qr dialog fixed

---
+++ frontend/src/components/qrdialog.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-dialog
+    v-model="isDialog"
+    max-width="800px">
+    <v-card>
+      <v-layout column>
+        <v-flex>
+          <v-spacer></v-spacer>
+          <v-btn
+            icon
+            color="red"
+            @click="$emit('closeDialog')">
+            <v-icon>close</v-icon>
+          </v-btn>
+        </v-flex>
+        <v-flex>
+          <qrcode-stream @decode="onDecode" />
+        </v-flex>
+      </v-layout>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import { QrcodeStream } from 'vue-qrcode-reader'
+
+export default {
+  name: 'qrdialog',
+  props: ['isDialog'],
+  data: () => ({
+  }),
+  methods: {
+    async onDecode(decodedString) {
+      const link = decodedString
+      this.$emit('closeDialog', link)
+    },
+  },
+  components: {
+    QrcodeStream
+  }
+}
+</script>
+
+<style>
+
+</style>
