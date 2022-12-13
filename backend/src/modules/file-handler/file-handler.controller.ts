2dc48e93cd98032fb6bb42fa8fa2bd66822e6527
elzuha <elzuha2000@gmail.com>
2022-12-13 11:45:30 +0300

    getting files available

--- backend/src/modules/file-handler/file-handler.controller.ts
+++ backend/src/modules/file-handler/file-handler.controller.ts
@@ -1,18 +1,21 @@
 import { Controller, Get, Param, StreamableFile, Res } from '@nestjs/common';
 import type { Response } from 'express';
-import { createReadStream } from 'fs';
+import { readFileSync, createReadStream } from 'fs';
 import { join } from 'path';
 
 @Controller('file-handler')
 export class FileHandlerController {
-  @Get(':giftCardId?')
-  buffer(@Res() res: Response, @Param() giftCardId: string): StreamableFile {
-    const pathToFile = join(process.cwd(), 'codes', giftCardId + '.png');
-    const file = createReadStream(pathToFile);
+  @Get(':id')
+  buffer(@Res() res: Response, @Param('id') id: string) {
+    const pathToFile = join(process.cwd(), 'codes', id + '.png');
+    const file = readFileSync(pathToFile)
+    res.contentType('image/png');
+    res.send(file);
+    /* const file = createReadStream(pathToFile);
     res.set({
       'Content-Type': 'image/png',
-      'Content-Disposition': `attachment; filename="${pathToFile}"`,
+      'Content-Disposition': `attachment; filename="${id + '.png'}"`,
     });
-    return new StreamableFile(file);
+    return new StreamableFile(file); */
   }
 }
